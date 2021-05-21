94ab6b827a75a90ae0e8546ae241a0ba4293837c
Eddie Lopez <ejlopez00@gmail.com>
2021-05-21 08:56:59 -0400

    tested router

--- cmd/api/router/router.go
+++ cmd/api/router/router.go
@@ -4,12 +4,15 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/elopez00/scale-backend/cmd/api/sdk/auth"
 	"github.com/elopez00/scale-backend/pkg/app"
+	"github.com/elopez00/scale-backend/cmd/api/sdk/plaid"
+	"github.com/elopez00/scale-backend/cmd/api/middleware"
 )
  
 func Get(app *app.App) *httprouter.Router {
 	mux := httprouter.New()
 	mux.POST("/onboard", auth.Onboard(app))
 	mux.GET("/login", auth.Login(app))
+	mux.GET("/getLinkToken", middleware.Authenticate(plaid.GetPlaidToken(app), app))
 
 	return mux
 }
