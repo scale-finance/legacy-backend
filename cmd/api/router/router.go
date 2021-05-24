fbd33062a417ac8b55fd7262d65d7141173a4048
Eddie Lopez <ejlopez00@gmail.com>
2021-05-23 21:04:41 -0400

    changed login to post

--- cmd/api/router/router.go
+++ cmd/api/router/router.go
@@ -12,7 +12,7 @@ import (
 func Get(app *app.App) *httprouter.Router {
 	mux := httprouter.New()
 	mux.POST("/v0/onboard", auth.Onboard(app))
-	mux.GET("/v0/login", auth.Login(app))
+	mux.POST("/v0/login", auth.Login(app))
 	mux.GET("/v0/getLinkToken", m.Authenticate(p.GetPlaidToken(app), app))
 	mux.GET("/v0/createAccessToken", m.Authenticate(p.CreateAccessToken(app), app))
 	mux.GET("/v0/", m.Authenticate(auth.AuthCheck(), app))
