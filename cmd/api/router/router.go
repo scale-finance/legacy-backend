9e49b432e467d9eb69ecaa3db5ad5eb817ac474f
Eddie Lopez <ejlopez00@gmail.com>
2021-06-03 16:44:38 -0400

    added createBudget endpoint to router

--- cmd/api/router/router.go
+++ cmd/api/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	m "github.com/elopez00/scale-backend/cmd/api/middleware"
 	"github.com/elopez00/scale-backend/cmd/api/sdk/auth"
+	b "github.com/elopez00/scale-backend/cmd/api/sdk/budgeting"
 	p "github.com/elopez00/scale-backend/cmd/api/sdk/plaid"
 	"github.com/elopez00/scale-backend/pkg/app"
 	"github.com/julienschmidt/httprouter"
@@ -14,6 +15,7 @@ func Get(app *app.App) *httprouter.Router {
 	mux.POST("/v0/onboard", auth.Onboard(app))
 	mux.POST("/v0/login", auth.Login(app))
 	mux.POST("/v0/exchangePublicToken", m.Authenticate(p.ExchangePublicToken(app), app))
+	mux.POST("/v0/createBudget", m.Authenticate(b.Create(app), app))
 	mux.GET("/v0/getLinkToken", m.Authenticate(p.GetPlaidToken(app), app))
 	mux.GET("/v0/getTransactions", m.Authenticate(p.GetTransactions(app), app))
 	mux.GET("/v0/getBalances", m.Authenticate(p.GetBalance(app), app))
